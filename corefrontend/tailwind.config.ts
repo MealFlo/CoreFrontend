069d6d24d859bf58d02b15f9b3c3b3c013b879dd
avikhemka <avikhemka@icloud.com>
2024-05-28 12:57:36 +0545

    chore: Update tailwind.config.ts with new transition property for margin

--- corefrontend/tailwind.config.ts
+++ corefrontend/tailwind.config.ts
@@ -83,6 +83,9 @@ const config: Config = {
         "accordion-up": "accordion-up 0.2s ease-out",
         "caret-blink": "caret-blink 1.25s ease-out infinite", // Add this new animation
       },
+      transitionProperty: {
+        'margin': 'margin'
+      }
     },
   },
   plugins: [
