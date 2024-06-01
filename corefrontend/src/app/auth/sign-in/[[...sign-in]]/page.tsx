f6ab00dbf758fe6cc65d6f131e02f0404703089d
avikhemka <avi@grdflo.com>
2024-06-02 04:38:08 +0545

    refactor: Add sign-in page and remove unused login and forgot password pages

---
+++ corefrontend/src/app/auth/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,26 @@
+
+import Link from "next/link"
+import Image from "next/image"
+import { SignUp } from "@clerk/nextjs";
+
+
+export default function signup() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-[100dvh] bg-gray-100 dark:bg-gray-900 px-4 md:px-6">
+      <div className="w-full max-w-md space-y-6">
+        <div className="flex items-center justify-center -ml-12">
+          <Link className="inline-flex items-center space-x-2" href="#">
+            <Image
+              src="/images/logo.webp" // Route of the image file
+              height={28} // Desired size with correct aspect ratio
+              width={48.15852048} // Desired size with correct aspect ratio
+              alt="Your Name"
+            />
+            <span className="text-2xl font-bold">MealFlow</span>
+          </Link>
+        </div>
+        <SignUp />
+      </div>
+    </div>
+  )
+}
