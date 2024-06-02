e98bb8c002815ecea5a2692e399e1014bb9fc97a
avikhemka <avi@grdflo.com>
2024-06-02 10:36:47 +0545

    refactor: Update middleware to protect non-public routes

--- corefrontend/src/middleware.ts
+++ corefrontend/src/middleware.ts
@@ -1,13 +1,17 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import {
+  clerkMiddleware,
+  createRouteMatcher
+} from '@clerk/nextjs/server';
 
-const isPublicRoute = createRouteMatcher(['/sign-in', '/sign-up']);
+const isProtectedRoute = createRouteMatcher([
+  '/',
+  '/kitchen(.*)',
+]);
 
-export default clerkMiddleware((auth, request) => {
-  if(!isPublicRoute(request)) {
-    auth().protect();
-  }
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-}
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
