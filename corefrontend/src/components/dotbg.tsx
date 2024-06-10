8a9f55e70f68397fe3f4cb6baacf2ab23dc26986
avikhemka <avi@grdflo.com>
2024-06-11 03:06:25 +0545

    refactor: Update DotBackground component to use absolute positioning for better responsiveness

--- corefrontend/src/components/dotbg.tsx
+++ corefrontend/src/components/dotbg.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function DotBackground() {
   return (
-    <div className="fixed top-0 left-0 h-screen w-screen dark:bg-black bg-neutral-100 dark:bg-dot-white/[0.2] bg-dot-black/[0.2] z-0">
+    <div className="absolute top-0 left-0 h-full w-full bg-neutral-100 dark:bg-dot-white/[0.2] bg-dot-black/[0.2] z-0">
       {/* Radial gradient for the container to give a faded look */}
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center">
         {/* Radial gradient element (if needed you can add it here) */}
